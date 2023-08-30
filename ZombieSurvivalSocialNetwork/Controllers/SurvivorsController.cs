5fc772fe52e2793c0a8c5e61161664f87cd65391
Sydwell.Mazibuko <69756153+SydwellBridges@users.noreply.github.com>
2023-08-30 22:47:58 +0200

    Create SurvivorsController with database context injection and route configuration

---
+++ ZombieSurvivalSocialNetwork/Controllers/SurvivorsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZombieSurvivalSocialNetwork.Data;
+
+namespace ZombieSurvivalSocialNetwork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurvivorsController : ControllerBase
+    {
+
+        private readonly ZombieSurvivalDbContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurvivorsController"/> class.
+        /// </summary>
+        /// <param name="context">The database context for survivor data.</param>
+        public SurvivorsController(ZombieSurvivalDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
