14668d79c130d884153db8730395ad39a6611ad1
Sydwell.Mazibuko <69756153+SydwellBridges@users.noreply.github.com>
2023-08-30 22:34:17 +0200

    Add zombie survival database context

---
+++ ZombieSurvivalSocialNetwork/Data/ZombieSurvivalDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZombieSurvivalSocialNetwork.Models;
+
+namespace ZombieSurvivalSocialNetwork.Data
+{
+    /// <summary>
+    /// Represents the database context for the Zombie Survival API.
+    /// </summary>
+    public class ZombieSurvivalDbContext : DbContext
+    {
+        /// <summary>
+        /// Gets or sets the DbSet of survivors.
+        /// </summary>
+        public DbSet<Survivor> Survivors { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ZombieSurvivalDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options for configuring the context.</param>
+        public ZombieSurvivalDbContext(DbContextOptions<ZombieSurvivalDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
